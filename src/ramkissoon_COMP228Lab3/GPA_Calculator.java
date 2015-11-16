c7ad042814d81a51cc686199f188adf68e12b7df
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-11-16 08:26:20 -0500

    added textbox for course code

--- src/ramkissoon_COMP228Lab3/GPA_Calculator.java
+++ src/ramkissoon_COMP228Lab3/GPA_Calculator.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -13,97 +14,93 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class GPA_Calculator extends JFrame{
+public class GPA_Calculator extends JFrame {
 	JButton calculateButton;
-	JComboBox <String>numOfCoursesCombo;
-	JComboBox <String>creditHoursCombo [] = new JComboBox [6];
-	JComboBox <String>gradeCombo [] = new JComboBox [6];
-	
-	JLabel creditsLabel, currentGPA_Label, 
-			numberOfCoursesLabel, 
-			itemNumLabel, courseCodeLabel, creditHoursLabel, gradeLabel,
-			finalGPA_Label;
-	
-	JLabel itemNum2_Label [] = new JLabel[6];
-	
-	JPanel topPanel, coursePanel, bottomPanel,
-			topPanelUpper, topPanelLower;
-	
-	JTextField creditsTxtField, currentGPA_TxtField;
-	
-	JTextField courseCodeTxtField [] = new JTextField[6];
+	JComboBox<String> numOfCoursesCombo;
+	//ArrayList<JTextField> courseCodeTextField;
+	JComboBox<String> creditHoursCombo[] = new JComboBox[6];
+	JComboBox<String> gradeCombo[] = new JComboBox[6];
 
-	public GPA_Calculator()
-	{
+	JLabel creditsLabel, currentGPA_Label, numberOfCoursesLabel, itemNumLabel, courseCodeLabel, creditHoursLabel,
+			gradeLabel, finalGPA_Label;
+
+	JLabel itemNum2_Label[] = new JLabel[6];
+
+	JPanel topPanel, coursePanel, bottomPanel, topPanelUpper, topPanelLower;
+
+	JTextField creditsTxtField, currentGPA_TxtField;
+
+	JTextField courseCodeTxtField[] = new JTextField[6];
+
+	public GPA_Calculator() {
 		// Frame by default have this, just to be explicit
-		// We will only be using North, Center, South , Collapsed will be East, West
-		setLayout( new BorderLayout()); 
-		
+		// Only using North, Center, South 
+		setLayout(new BorderLayout());
+
 		// Top Panel +++++++++++++++++++++++++++++++++++++++++++++++++
-		topPanel = new JPanel();		
+		topPanel = new JPanel();
 		topPanelUpper = new JPanel();
 		topPanelLower = new JPanel();
-		
+
 		// Top Panel Labels and Fields
-		creditsLabel = new JLabel ("Credit Hours Earned:");
-		creditsTxtField = new JTextField (4);
-		currentGPA_Label = new JLabel ("Current GPA:");
-		currentGPA_TxtField= new JTextField (4);
+		creditsLabel = new JLabel("Credit Hours Earned:");
+		creditsTxtField = new JTextField(4);
+		currentGPA_Label = new JLabel("Current GPA:");
+		currentGPA_TxtField = new JTextField(4);
 		numberOfCoursesLabel = new JLabel("Number of Courses:");
-		String [] courses = {"0", "1", "2", "3", "4", "5", "6"};
-		numOfCoursesCombo = new JComboBox <String>(courses);
-		
+		String[] courses = { "0", "1", "2", "3", "4", "5", "6" };
+		numOfCoursesCombo = new JComboBox<String>(courses);
+
 		// Top Panel Layout
 		topPanel.setLayout(new BorderLayout());
 		topPanelUpper.add(creditsLabel);
 		topPanelUpper.add(creditsTxtField);
 		topPanelUpper.add(currentGPA_Label);
 		topPanelUpper.add(currentGPA_TxtField);
-		
+
 		topPanel.add("North", topPanelUpper);
-		
+
 		topPanelLower.add(numberOfCoursesLabel);
 		topPanelLower.add(numOfCoursesCombo);
-		numOfCoursesCombo.addActionListener(new ComboHandler());	// listener for number of courses combo box
-		
+		// listener for courses combo box
+		numOfCoursesCombo.addActionListener(new ComboHandler()); 
+
 		topPanel.add("South", topPanelLower);
 		
-		
-		//	Course Panel +++++++++++++++++++++++++++++++++++++++++++++++++
+
+		// Course Panel +++++++++++++++++++++++++++++++++++++++++++++++++n  n  
 		coursePanel = new JPanel();
-		
+
 		// Course Panel Labels and Fields
 		itemNumLabel = new JLabel("Nr");
 		courseCodeLabel = new JLabel("Course code");
 		creditHoursLabel = new JLabel("Credit hours");
-		gradeLabel = new JLabel ("Grade");
-		
+		gradeLabel = new JLabel("Grade");
+
 		// Course Panel Layout
-		coursePanel.setLayout(new GridLayout(0, 4));	// specify only Columns
+		coursePanel.setLayout(new GridLayout(0, 4)); // specify only Columns
 		coursePanel.add(itemNumLabel);
 		coursePanel.add(courseCodeLabel);
 		coursePanel.add(creditHoursLabel);
 		coursePanel.add(gradeLabel);
-		
-		// Push labels up with empty cells 24 is an arbitrary # 
-		for(int i = 0; i < 24; i++){
+
+		// Push labels up with 24 empty cells (arbitrary #)
+		for (int i = 0; i < 24; i++) {
 			coursePanel.add(new JLabel());
 		}
-		
+
 		// Bottom Panel +++++++++++++++++++++++++++++++++++++++++++++++++
 		bottomPanel = new JPanel();
-		
-		
+
 		// Bottom Panel Label and Button
-		finalGPA_Label = new JLabel ("Final GPA");
-		calculateButton = new JButton ("Calculate");
-		
+		finalGPA_Label = new JLabel("Final GPA");
+		calculateButton = new JButton("Calculate");
+
 		// Bottom Panel Layout
-		bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));		
+		bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		bottomPanel.add(finalGPA_Label);
 		bottomPanel.add(calculateButton);
-		
-		
+
 		// Add Top, Course, Bottom Panels to Frame
 		add("North", topPanel);
 		add("Center", coursePanel);
@@ -113,48 +110,38 @@ public class GPA_Calculator extends JFrame{
 	public static void main(String[] args) {
 		GPA_Calculator frame = new GPA_Calculator();
 		frame.setTitle("GPA Calculator");
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		frame.setSize(480,320);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(480, 320);
 		frame.setVisible(true);
+		
+		ArrayList<JTextField> courseCodeTextField = new ArrayList<JTextField>();
 	}
-	
-	private class ComboHandler implements ActionListener{
+
+	private class ComboHandler implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == numOfCoursesCombo){
+			if (e.getSource() == numOfCoursesCombo) {
 				coursePanel.removeAll();
 				coursePanel.add(itemNumLabel);
 				coursePanel.add(courseCodeLabel);
 				coursePanel.add(creditHoursLabel);
 				coursePanel.add(gradeLabel);
-				for (int i = 0; i < numOfCoursesCombo.getSelectedIndex(); i++){
-					coursePanel.add(new JLabel(Integer.toString(i+1)));
-					coursePanel.add(new JLabel("X"));
+				
+				for (int i = 0; i < numOfCoursesCombo.getSelectedIndex(); i++) {
+					//Course Number Column
+					coursePanel.add(new JLabel(Integer.toString(i + 1)));
+					
+					// Course Code Column
+					
+					coursePanel.add(new JTextField("X"));
 					coursePanel.add(new JLabel("X"));
 					coursePanel.add(new JLabel("X"));
 				} // end for
-				
-				coursePanel.revalidate();	// refresh layout, better than repaint()
-			} //end if			
-		} // end actionPerformed		
-	}	
+
+				coursePanel.revalidate(); // refresh layout, better than
+											// repaint()
+			} // end if
+		} // end actionPerformed
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
