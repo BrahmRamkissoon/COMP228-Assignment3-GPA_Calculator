493f5e7fc5b2664ded248c396df34232cfdc2ef9
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-11-16 13:04:38 -0500

    added setGrade Method and tested finalGPA label

--- src/ramkissoon_COMP228Lab3/GPA_Calculator.java
+++ src/ramkissoon_COMP228Lab3/GPA_Calculator.java
@@ -11,11 +11,12 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GPA_Calculator extends JFrame {
-	JButton calculateButton;
+	JButton calculateGPA_Button;
 	JComboBox<String> numOfCoursesCombo;
 	//ArrayList<JTextField> courseCodeTextField;
 	JComboBox<String> creditHoursCombo[] = new JComboBox[6]; // ComboBox to hold credit hours
@@ -23,8 +24,10 @@ public class GPA_Calculator extends JFrame {
 	
 	private static final String[] courses = { "0", "1", "2", "3", "4", "5", "6" };
 	private static final String[] creditHours = {"3", "4", "5"};
-	private static final String[] letterGrade = { "A+ 90-100%", "A 80-89%", "B+ 75-79%", "B 70-74%",
-													"C+ 65-69%", "C 60-64%", "D+ 55-59", "D 50-54%", "F 0-49%"};
+	private static final String[] letterGrade = { "A+ 90-100%", "A   80-89%", "B+ 75-79%", "B   70-74%",
+													"C+ 65-69%", "C   60-64%", "D+ 55-59", "D   50-54%", "F   0-49%"};
+	
+	private double grade;
 	
 	JLabel creditsLabel, currentGPA_Label, numberOfCoursesLabel, itemNumLabel, courseCodeLabel, creditHoursLabel,
 			gradeLabel, finalGPA_Label;
@@ -97,13 +100,16 @@ public class GPA_Calculator extends JFrame {
 		bottomPanel = new JPanel();
 
 		// Bottom Panel Label and Button
-		finalGPA_Label = new JLabel("Final GPA");
-		calculateButton = new JButton("Calculate");
+		finalGPA_Label = new JLabel("Final GPA: ");
+		calculateGPA_Button = new JButton("Calculate");
 
 		// Bottom Panel Layout
 		bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		bottomPanel.add(finalGPA_Label);
-		bottomPanel.add(calculateButton);
+		bottomPanel.add(calculateGPA_Button);
+		// listener for courses combo box
+		calculateGPA_Button.addActionListener(new ButtonHandler());
+				 
 
 		// Add Top, Course, Bottom Panels to Frame
 		add("North", topPanel);
@@ -134,8 +140,7 @@ public class GPA_Calculator extends JFrame {
 				
 				for (int i = 0; i < numOfCoursesCombo.getSelectedIndex(); i++) {
 					//Course Number Column
-					coursePanel.add(new JLabel(Integer.toString(i + 1)));
-					
+					coursePanel.add(new JLabel(Integer.toString(i + 1)));					
 					// Course Code Column					
 					coursePanel.add(new JTextField("X"));
 					// Credit Hours Column
@@ -149,4 +154,48 @@ public class GPA_Calculator extends JFrame {
 			} // end if
 		} // end actionPerformed
 	}
+	
+	private class ButtonHandler implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == calculateGPA_Button) {	
+							
+				
+				finalGPA_Label.setText("Final GPA: test");
+				
+				} // end for
+
+				revalidate(); // refresh layout, better than
+											// repaint()
+			} // end if
+		} // end actionPerformed
+	
+	public double setGrade(String g){			
+					switch (g) {
+					case "A+ 90-100%":
+						return grade = 4.5;
+					case "A   80-89%":
+						return grade = 4.0;											
+					case "B+ 75-79%":
+						return grade = 3.5;						
+					case "B   70-74%":
+						return grade = 3.0;
+					case "C+ 65-69%":
+						return grade = 2.5;
+					case "C   60-64%":
+						return grade = 2.0;
+					case "D+ 55-59":
+						return grade = 1.5;
+					case "D   50-54%":
+						return grade = 1.0;
+					case "F   0-49%":
+						return grade = 0.0;
+					default:
+						return -1.0;						
+						}		
+		 }
 }
+	
+	
+
